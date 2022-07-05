d773fcff05b390a8a71c8481caffdfeacf5686ef
Pedro <josepedrobernardo@hotmail.com>
2022-07-05 10:24:21 -0300

    Inclusão do Retrofit e Picasso

--- app/src/main/java/com/example/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/com/example/soccernews/ui/news/NewsViewModel.java
@@ -4,25 +4,50 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.soccernews.data.remote.SoccerNewsApi;
 import com.example.soccernews.domain.News;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<List<News>> news;
+    private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final SoccerNewsApi api;
 
     public NewsViewModel() {
-        this.news = new MutableLiveData<>();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://pedrobernardo27.github.io/soccer-news-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        //TODO Remover Mock de Noticias
-        List<News> news = new ArrayList<>();
-        news.add(new News("Ferroviaria tem desfalque importante.","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
-        news.add(new News("Ferrinha joga no sábado","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"));
-        news.add(new News("Copa do mundo feminina inicia","t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is"));
+        api = retrofit.create(SoccerNewsApi.class);
+        this.findNews();
 
-        this.news.setValue(news);
+    }
+
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+                if (response.isSuccessful()){
+                    news.setValue(response.body());
+                } else{
+                    //todo pensar
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+                //TODO Pensar
+            }
+        });
     }
 
     public LiveData<List<News>> getNews() {
